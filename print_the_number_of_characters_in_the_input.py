1d1e82ace02da712a0246c2d876667769bfd8641
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-21 02:15:47 +0800

    coding_print_number_of_charc_in_input

---
+++ print_the_number_of_characters_in_the_input.py
@@ -0,0 +1,4 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+full_name = input("Enter your fullname: ")
+letter_counter = len(full_name)
+print(f"Output: {letter_counter}")
