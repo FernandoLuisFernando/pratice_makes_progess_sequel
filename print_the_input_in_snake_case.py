d0e89a2a21201c7d5db37738a1105421014ffeb0
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-21 02:23:45 +0800

    coding_print_input_in_snake_case

---
+++ print_the_input_in_snake_case.py
@@ -0,0 +1,4 @@
+# Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+full_name = input("Enter your fullname: ")
+snake_full_name = full_name.title().replace(" ", "_")
+print(f"Output: {snake_full_name}")
