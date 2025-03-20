6069068d5c94f5bca6dd61502c884a4fe5bfd0be
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-21 01:33:13 +0800

    coding_print_input_names_all_caps

---
+++ print_the_input_in_all_capital_letter.py
@@ -0,0 +1,4 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+full_name = input("Enter your fullname: ")
+capitalized_name = full_name.upper()
+print("Output:", capitalized_name)
